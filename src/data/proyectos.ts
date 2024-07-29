5d44a4577ec65c946af78a909059c4367411b20e
Francisco henriquez <francisco96.henriquez@gmail.com>
2024-07-29 06:17:37 -0400

    primer commit

---
+++ src/data/proyectos.ts
@@ -0,0 +1,14 @@
+
+export const proyectos = [
+  {
+    nombre: 'Therappyhub',
+    imagen: 'https://i.pinimg.com/originals/94/3b/92/943b92b7802520128d38533de04c0c34.png',
+    tecnologias: ['Astro', 'Tailwind', 'React']
+  },
+  {
+    nombre: 'Clinibook',
+    imagen: 'https://i.pinimg.com/originals/25/b2/cf/25b2cffcb3fc28b006d0d5ff1e8f796e.png',
+    tecnologias: ['Astro', 'Tailwind']
+  },
+];
+
